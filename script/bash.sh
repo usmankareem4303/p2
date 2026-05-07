0be17f0dd8a8adbf0e4aba34fde25a0f6c32f010
usmankareem4303 <www.usmankareem@gmail.com>
2026-05-07 21:41:11 +0100

    temp

---
+++ script/bash.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Use default values if environment variables are not set
+USER_NAME="${USER_NAME:-John}"
+LAST_NAME="${LAST_NAME:-Doe}"
+AGE="${AGE:-30}"
+FAVORITE_COLOR="${FAVORITE_COLOR:-blue}"
+FAVORITE_FOOD="${FAVORITE_FOOD:-rice}"
+FAVORITE_MOVIE="${FAVORITE_MOVIE:-Inception}"
+
+echo "Hello $USER_NAME"
+echo "Hello $USER_NAME $LAST_NAME"
+echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old"
+echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old and your favorite color is $FAVORITE_COLOR"
+echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR and your favorite food is $FAVORITE_FOOD"
+echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR, your favorite food is $FAVORITE_FOOD and your favorite movie is $FAVORITE_MOVIE"
+
+# to test git merge and stash
+## sunday
+
+echo "hellloooo"
+echo "hello"echo "Uthman made an update here 💻"
+date
+echo " are you working now"
+
+echo "what is your brother name"
+echo "do you have a brother"
+echo "do you have a sister"
+echo "yes my brother name is uthman and my sister name is fatima"
